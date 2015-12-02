f3d80e5b73a77cd431c941a86a5bb0cc3e5b3e14
Jeff Minnear <jeffminnear@gmail.com>
2015-12-02 08:18:50 -0500

    Add email host to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  # config.action_mailer.default_url_options = { host: '##NAME#OF#APP##.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://within-a-week.herokuapp.com' }
 end
