110b1691fa7addf75dfb9958cf2874c8a34ba5f0
Jeff Minnear <jeffminnear@gmail.com>
2015-12-04 12:20:11 -0500

    Clean up and remove unused files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'goals/create'
-
   devise_for :users
 
   resources :users, only: :show do
--- spec/views/goals/create.html.erb_spec.rb
+++
@@ -1,4 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "goals/create.html.erb", type: :view do
-end
