08a5925bb2e50e57bf7f0e5e83b4576291f3b339
Jeff Minnear <jeffminnear@gmail.com>
2015-12-07 16:28:52 -0500

    Change color of goal as it approaches expiration date

--- app/helpers/goals_helper.rb
+++ app/helpers/goals_helper.rb
@@ -3,4 +3,15 @@ module GoalsHelper
     time_of_expiration = goal.created_at + 7.days
     time_left_until_expiration = distance_of_time_in_words(Time.now, time_of_expiration)
   end
+
+  def urgency(goal)
+    time_elapsed = (Time.now - goal.created_at) / 86400
+    if time_elapsed > 6
+      return "danger"
+    elsif time_elapsed > 4
+      return "warning"
+    else
+      return "ok"
+    end
+  end
 end
