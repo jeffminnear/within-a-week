e68fa967abace29fb86d730795e3a24b66d7f8c2
Jeff Minnear <jeffminnear@gmail.com>
2015-12-07 15:13:29 -0500

    Clean up and clarify goals_helper.rb

--- app/helpers/goals_helper.rb
+++ app/helpers/goals_helper.rb
@@ -1,11 +1,6 @@
 module GoalsHelper
-  def time_left_until_expires(goal)
+  def time_left_to_complete(goal)
     time_of_expiration = goal.created_at + 7.days
-    if Time.now < time_of_expiration
-      time_left_until_expiration = distance_of_time_in_words(Time.now, time_of_expiration)
-      return "#{time_left_until_expiration} left to complete"
-    else
-      return "EXPIRED!"
-    end
+    time_left_until_expiration = distance_of_time_in_words(Time.now, time_of_expiration)
   end
 end
