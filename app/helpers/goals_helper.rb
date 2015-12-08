dab53f7a6d81c5a181c79919b76e89b748bd04f9
Jeff Minnear <jeffminnear@gmail.com>
2015-12-08 12:33:09 -0500

    Update goals_helper.rb to use constants

--- app/helpers/goals_helper.rb
+++ app/helpers/goals_helper.rb
@@ -1,14 +1,18 @@
 module GoalsHelper
+  TIME_ALLOTTED_TO_COMPLETE_A_GOAL_IN_DAYS  = 7.days
+  DANGER_THRESHOLD_IN_DAYS                  = 6
+  WARNING_THRESHOLD_IN_DAYS                 = 4
+
   def time_left_to_complete(goal)
-    time_of_expiration = goal.created_at + 7.days
+    time_of_expiration = goal.created_at + TIME_ALLOTTED_TO_COMPLETE_A_GOAL_IN_DAYS
     time_left_until_expiration = distance_of_time_in_words(Time.now, time_of_expiration)
   end
 
   def urgency(goal)
-    time_elapsed = (Time.now - goal.created_at) / 1.day
-    if time_elapsed > 6
+    time_elapsed_in_days = (Time.now - goal.created_at) / 1.day
+    if time_elapsed_in_days > DANGER_THRESHOLD_IN_DAYS
       return "danger"
-    elsif time_elapsed > 4
+    elsif time_elapsed_in_days > WARNING_THRESHOLD_IN_DAYS
       return "warning"
     else
       return "ok"
---
+++ spec/helpers/goals_helper_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+RSpec.describe GoalsHelper, type: :helper do
+  xdescribe "time_left_to_complete(goal)" do
+  end
+end
