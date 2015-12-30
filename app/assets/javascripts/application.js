e6d688fdfb2962e1b4cd34efdc60794191ce3d9b
Jeff Minnear <jeffminnear@gmail.com>
2015-12-30 10:19:54 -0500

    Add JS snippet to use Do-Tell

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,12 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+var doTell = {};
+  doTell.report = function(eventName){
+    var event = {event: { name: eventName}};
+
+    var request = new XMLHttpRequest();
+    request.open("POST", "https://do-tell-sir.herokuapp.com/api/events", true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify(event));
+  }
