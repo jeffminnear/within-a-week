37702420f2890112b3cc70d7453b2de30add551b
Jeff Minnear <jeffminnear@gmail.com>
2016-01-13 09:47:53 -0500

    Add Do-Tell report User Registered

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
 
-  def sign_up(resource_name, resource)
+  def after_inactive_sign_up_path_for(resource)
     flash[:event] = "User Registers"
     super
   end
