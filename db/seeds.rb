a9cca77d35d986b24b5103796f38a41347c63a24
Jeff Minnear <jeffminnear@gmail.com>
2016-01-20 10:37:38 -0500

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,17 @@
+# Create Teams
+5.times do
+  team = Team.create!
+end
+teams = Team.all
+
 # Create Users
 20.times do
   user = User.create!(
     email:            RandomData.random_email,
     password:         RandomData.random_password,
     confirmed_at:     RandomData.random_time,
-    last_sign_in_at:  RandomData.random_time
+    last_sign_in_at:  RandomData.random_time,
+    team:             teams.sample
   )
 end
 users = User.all
@@ -22,9 +29,11 @@ end
 my_user = User.create!(
   email: "jeffminnear@gmail.com",
   password: "helloworld",
-  confirmed_at: Time.now
+  confirmed_at: Time.now,
+  team: teams.sample
 )
 
 puts "Seed Finished"
+puts "#{Team.count} teams created"
 puts "#{User.count} users created"
 puts "#{Goal.count} goals created"
